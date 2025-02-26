9aaae67dfe0a3e308dda078df25222cc1745a9c5
Sidiqa Fekrat <Sfekrat@depaul.edu>
2025-02-26 10:26:50 -0600

    Update motor_helpers.cpp

--- src/motor_helpers.cpp
+++ src/motor_helpers.cpp
@@ -1,1 +1,21 @@
+#include "motor_helpers.h"
+#include "SCMD.h"
+
+SCMD myMotorDriver;
+
+void motorInit() {
+    myMotorDriver.settings.commInterface = I2C_MODE;
+    myMotorDriver.settings.I2CAddress = 0x5D;
+    myMotorDriver.settings.chipSelectPin = 10;
+
+    while (myMotorDriver.ready() == false);
+    Serial.println("Motor initialized");
+
+    myMotorDriver.inversionMode(1, 1);
+    myMotorDriver.enable();
+}
+
+void controlMotor(uint8_t motor, uint8_t direction, uint8_t speed) {
+    myMotorDriver.setDrive(motor, direction, speed);
+}
 
