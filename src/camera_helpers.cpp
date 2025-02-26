4a448585114b431e5f3ead0587989c0036dfba5b
Sidiqa Fekrat <Sfekrat@depaul.edu>
2025-02-26 10:25:55 -0600

    Update camera_helpers.cpp

--- src/camera_helpers.cpp
+++ src/camera_helpers.cpp
@@ -1,1 +1,72 @@
 
+#include "camera_helpers.h"
+#include "esp_camera.h"
+
+static bool is_initialised = false;
+
+bool cameraInit() {
+    if (is_initialised) return true;
+
+    camera_config_t camera_config = {
+        .pin_pwdn = PWDN_GPIO_NUM,
+        .pin_reset = RESET_GPIO_NUM,
+        .pin_xclk = XCLK_GPIO_NUM,
+        .pin_sscb_sda = SIOD_GPIO_NUM,
+        .pin_sscb_scl = SIOC_GPIO_NUM,
+        .pin_d7 = Y9_GPIO_NUM,
+        .pin_d6 = Y8_GPIO_NUM,
+        .pin_d5 = Y7_GPIO_NUM,
+        .pin_d4 = Y6_GPIO_NUM,
+        .pin_d3 = Y5_GPIO_NUM,
+        .pin_d2 = Y4_GPIO_NUM,
+        .pin_d1 = Y3_GPIO_NUM,
+        .pin_d0 = Y2_GPIO_NUM,
+        .pin_vsync = VSYNC_GPIO_NUM,
+        .pin_href = HREF_GPIO_NUM,
+        .pin_pclk = PCLK_GPIO_NUM,
+        .xclk_freq_hz = 20000000,
+        .ledc_timer = LEDC_TIMER_0,
+        .ledc_channel = LEDC_CHANNEL_0,
+        .pixel_format = PIXFORMAT_JPEG,
+        .frame_size = FRAMESIZE_QVGA,
+        .jpeg_quality = 12,
+        .fb_count = 1,
+        .fb_location = CAMERA_FB_IN_PSRAM,
+        .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+    };
+
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x\n", err);
+        return false;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    if (s->id.PID == OV3660_PID) {
+        s->set_vflip(s, 1);
+        s->set_brightness(s, 1);
+        s->set_saturation(s, 0);
+    }
+
+    is_initialised = true;
+    return true;
+}
+
+bool cameraCapture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Failed to capture image");
+        return false;
+    }
+
+    // Handle image conversion and resizing
+    bool converted = fmt2rgb888(fb->buf, fb->len, PIXFORMAT_JPEG, out_buf);
+    esp_camera_fb_return(fb);
+
+    if (!converted) {
+        Serial.println("Conversion failed");
+        return false;
+    }
+
+    return true;
+}
